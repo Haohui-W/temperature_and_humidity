201a96ad7c6bede889d100f932b5983b84a0f8a0
haohui <haohui-w@qq.com>
2026-05-09 21:43:48 +0800

    Configure release signing properties

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,8 +26,19 @@ android {
         }
     }
 
+    signingConfigs {
+        create("release") {
+            storeFile = file(project.property("RELEASE_STORE_FILE") as String)
+            storePassword = project.property("RELEASE_STORE_PASSWORD") as String?
+            keyAlias = project.property("RELEASE_KEY_ALIAS") as String?
+            keyPassword = project.property("RELEASE_KEY_PASSWORD") as String?
+            enableV2Signing = true
+        }
+    }
+
     buildTypes {
         release {
+            signingConfig = signingConfigs.getByName("release")
             isMinifyEnabled = false
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
